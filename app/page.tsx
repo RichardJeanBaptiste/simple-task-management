94e6a3581ecd6cfe69e6c1cdc402ddd6b8b29608
Richard Jean-Baptiste <Richinbk1@gmail.com>
2024-03-07 05:38:00 -0500

    created board title component/ created TaskBox compoennt

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,41 @@
 "use client"
 
 import React, {useEffect, useState} from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Typography, Box, IconButton } from '@mui/material';
+import { Box, IconButton, TextField } from '@mui/material';
+import { useTheme }  from '@mui/material/styles';
 import { useRouter } from 'next/navigation';
 import { v4 as uuidv4 } from 'uuid';
 import EditIcon from '@mui/icons-material/Edit';
-import logo from "@/public/Logo.svg";
+import logo from "@/public/Logo.svg";;
 import Image from 'next/image';
 import "./page.module.css";
 
-
-
-const theme = createTheme({
-  typography: {
-    fontFamily: 'Outfit, sans-serif', // Specify your desired font family
-    // You can also customize other typography properties here
+const useStyles = (theme: any) => ({
+  root: {
+    width: '50%',
+    height: '80vh',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
   },
-});
+  root2: {
+    width: '100%',
+    height: '100%',
+    position: 'relative'
+  }
+
+})
 
 export default function Home() {
 
+  const theme = useTheme();
   const router = useRouter();
+  const styles = useStyles(theme);
 
   const [id, SetNewID] = useState(uuidv4());
+  const [editTitle, SetEditTitle] = useState(false);
+  
 
   useEffect(() => {
 
@@ -55,29 +67,53 @@ export default function Home() {
     )
   }
 
+  const BoardTitle = () => {
+    const [title, SetTitle] = useState("My Task Board");
+
+    if(!editTitle){
+      return (
+        <Box>
+          <h3>{title}</h3>
+          <h5>Tasks to keep organised</h5>
+        </Box>
+      )
+    } else {
+      return (
+        <Box>
+            <TextField id="standard-basic" label="Board Name" variant="standard" placeholder={title}/>
+            <h5>Tasks to keep organised</h5>
+        </Box>
+      )
+    }
+    
+  }
+
   return (
-    <Box>
-      <p>
-        <span style={{paddingRight: '2%'}}>
-          <Image
-            src={logo}
-            width={50}
-            height={50}
-            alt="logo"
-          />
-        </span> 
-          My Task Board 
-        <span style={{ paddingLeft: '.5%'}}>
-          <IconButton>
-              <EditIcon/>
-          </IconButton>
-        </span>
-        </p>
-      <Typography variant="body1">Tasks to keep organised</Typography>
+    <Box sx={styles.root}>
+      <Box sx={styles.root2}>
+        <Box sx={{ display: 'flex', flexDirection:"row", width: '100%'}}>
+            <Box sx={{width:'50px', height:'50px', paddingRight: '2%', position: 'relative'}}>
+              <Image
+                src={logo}
+                fill
+                alt="logo"
+              />
+            </Box>
 
-      <TaskBox color="orange" icon="test"/>
+            <BoardTitle/>
 
-      <TaskBox color="orange" icon="test" new_task="true"/>
+            <IconButton onClick={() => SetEditTitle(!editTitle)}>
+                <EditIcon/>
+            </IconButton>
+
+            
+        </Box>
+        
+
+        <TaskBox color="orange" icon="test"/>
+
+        <TaskBox color="orange" icon="test" new_task="true"/>
+      </Box>
     </Box>    
   );
 }
